6a7a3d373d79d1b02852eca5e42d786fc6ab0a8b
Emre Yuz <emreyuz@gmail.com>
2021-10-19 00:02:13 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,24 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+dolarDun = "İstanbul"
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+// euroDun = 11 const'un değerini değiştiremezsin. değişken değil, değişmez bu.
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut kredisi", "Emlak konut kredisi", "Kamu konut kredisi"]
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+
+console.log(konutKredileri)
